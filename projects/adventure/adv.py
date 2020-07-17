1d973b97b578ff0b7b91c0f5f7c9e74c976a3f33
andronikmk <andronikmk@gmail.com>
2020-07-17 14:38:38 -0400

    Add comments and print statements

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -34,38 +34,44 @@ traversal_path = []
 # visited rooms
 rooms = {}
 
+# backtracking list
 reverse_path = []
 
 # opposite direction
 reverse_dir = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 
 # Add room zero to rooms
+# so index is not out of range
 rooms[0] = player.current_room.get_exits()
+
 
 # While the number of rooms that you have visisted is less than the total amount of room
     # and if player hasn't visisted the room
         # get the exits
 while len(rooms) < len(room_graph)-1:
-    if player.current_room.id not in rooms:
-        # Add exits for current room to rooms
+    player_id = player.current_room.id
+    print("1. CURRENT ROOM ID: ", player_id)
+    if player_id not in rooms:
+        # Node is looking around and asking which way is the exit.
         rooms[player.current_room.id] = player.current_room.get_exits()
-        print("These are the exits: ", player.current_room.get_exits_string())
-        # similar to prev in other assignment. getting last direction traveled
+        print("2. EXITS: ", player.current_room.get_exits_string())
+        # Grab the last direction traveled and it shoud be a possible exit.
         last_dir = reverse_path[-1]
-        print("Last direction traveld: ", last_dir)
-        rooms[player.current_room.id].remove(last_dir)
-
+        print("3. LAST DIRECTION TRAVELED: ", last_dir)
+        # And remove the last exit
+        x = rooms[player.current_room.id].remove(last_dir)
+        print("4. REMOVE LAST EXIT", x)
     # when there are no more rooms left to traverse do
     while len(rooms[player.current_room.id]) < 1:
         r = reverse_path.pop()
-        print("Pop last dir in reverse path:", r)
+        print("5. POP LAST DIRECTION IN THE REVERSE STRING:", r)
         # travel down the opposite path now
         # after you add it to the traversal path
         traversal_path.append(r)
         player.travel(r)
 
     exit_dir = rooms[player.current_room.id].pop(0)
-    print("First exit direction in room: ",exit_dir)
+    print("6. TRAVEL DOWN FIRST POSSIBLE EXIT: ",exit_dir)
     # append to traversal path
     traversal_path.append(exit_dir)
     # add reverse direction
